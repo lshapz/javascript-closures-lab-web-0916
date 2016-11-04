be3945940e22e93a1ee8218fee33dcef5544a3f6
lshapz <laurashapiro01@gmail.com>
2016-11-04 17:17:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+var counter = 0
+
+function addBump(){counter +=1}
+function getBumps(){return counter}
+
+return {
+  addBump,
+  getBumps,
+};
+
+}
+
+function createAnimal(animalType){
+  return function createAnimal(deadlyDevice){
+    return {'animalType': animalType, 'deadlyDevice': deadlyDevice }
+  }
+
+}
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
